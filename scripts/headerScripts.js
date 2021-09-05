2149d7ed0458c46ee1d90f2199ba25b05df17eb3
Alexander Nartsev <sainte-man@yandex.ru>
2021-09-05 15:25:12 +0300

    fix header js

--- scripts/headerScripts.js
+++ scripts/headerScripts.js
@@ -1,19 +1,33 @@
 const page = document.querySelector('.page');
 const menu = page.querySelector('.header__nav');
-const elementsArr = menu.querySelectorAll('.header__list-el_type_menu');
+const point = menu.querySelector('.header__list-el_point-on');
+const buttonProjects = page.querySelector('.button__content_type_projects');
 const buttonBurger = page.querySelector('.button_type_burger');
+const elementsArr = menu.querySelectorAll('.header__list-el_type_menu');
 
-menu.addEventListener('click', (event) => {
+
+function displayMobileMenu() {
+
+    if(!menu.classList.contains('header__nav_mobile')){
+
+        menu.classList.add('header__nav_mobile');
+
+    } else {
+
+        menu.classList.remove('header__nav_mobile');
+
+    }
+
+};
+
+function setPoint(event) {
 
     let target = event.target;
     let parent = target.closest('.header__list-el_type_menu');
 
     if(!parent.classList.contains('header__list-el_point-on')) {
 
-        const buttonProjects = page.querySelector('.button__content_type_projects');
-        const point = menu.querySelector('.header__list-el_point-on');
-
-        buttonProjects.textContent = point.textContent
+        buttonProjects.textContent = point.textContent;
 
         for(i = 0; i < elementsArr.length; i++) {
 
@@ -25,10 +39,7 @@ menu.addEventListener('click', (event) => {
 
     }
 
-});
+};
 
-buttonBurger.addEventListener('click', () => {
-
-    menu.classList.toggle('header__nav_mobile');
-
-});
+buttonBurger.addEventListener('click', displayMobileMenu);
+menu.addEventListener('click', setPoint);
